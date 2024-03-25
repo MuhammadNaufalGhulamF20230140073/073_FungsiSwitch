b4b25e56e96c432232c9844fabbe54b285efe4db
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-03-25 15:10:15 +0700

    penambahan CLS

--- 073_FungsiSwitch.cpp
+++ 073_FungsiSwitch.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int bil1, bil2;
+
 void input()
 {
     cout << "Masukan Bilangan Pertama =";
@@ -45,37 +46,44 @@ int main()
         cout << "4.Perkalian " << endl;
         cout << "5.Exit" << endl;
         cout << "Masukkan pilihan 1/2/3/4/5 = ";
-        cin >> pilihan;     
-    } while (pilihan !=5);
-    
-    switch (pilihan)
+        cin >> pilihan;
+
+        switch (pilihan)
 {
 
     case 1:
     input();
     cout << "Hasil Penjumblahan = " << penjumblahan(bil1,bil2) << endl;
+    system("pause");
     break;
 
     case 2:
     input();
     cout << "Hasil Penmbagian = " << pembagian(bil1,bil2) << endl;
+    system("pause");
     break;
 
     case 3:
     input();
     cout << "Hasil Pengurangan = " << pengurangan(bil1,bil2) << endl;
+    system("pause");
     break;
 
     case 4:
     input();
     cout << "Hasil Perkalian = " << perkalian(bil1,bil2) << endl;
+    system("pause");
     break;
 
     case 5:
     break;
 
+    default : cout <<"pilihan tidak ada" << endl;
+    break;
 }
-
-}
-
+system("CLS");
+}     while (pilihan !=5);
+    }
+  
+    
 
